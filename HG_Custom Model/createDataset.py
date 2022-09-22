40a32d103988ccd2d4750fd18b02bfe51a7ca430
= <parulchutani.phd@gmail.com>
2022-09-22 12:28:11 +0530

    Custom Model for Custom dataset

---
+++ HG_Custom Model/createDataset.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import time
+
+class_name = input("Enter Class Name : ")
+path = r"C:\Users\parul.chutani\opencv3hrs\Resources\Dataset/%s/" %class_name
+webcam = cv2.VideoCapture(0)
+counter = 0
+
+while True:
+    success, frame = webcam.read()
+    cv2.imshow("Video", frame)
+
+    if os.path.exists(path):
+        # while (counter<=100):
+        os.chdir(path)
+        # filename = class_name + '_' + str(counter) + '.jpg'
+        cv2.imwrite(class_name + '_' + str(counter) + '.jpg', frame )
+        time.sleep(0.3)
+        counter+=1
+    else:
+        os.mkdir(path)
+        filename = class_name + '_' + str(counter) + '.jpg'
+        cv2.imwrite(filename, frame)
+        counter += 11
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+webcam.release()
+cv2.destroyAllWindows()
